5d1a014d90acfd2390f534dee7acd6d60accd778
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-11-20 20:47:13 +0700

    fix: add CORS after deploying

--- src/main/java/com/example/hackathon/config/SecurityConfig.java
+++ src/main/java/com/example/hackathon/config/SecurityConfig.java
@@ -27,62 +27,64 @@ import java.util.List;
 @EnableWebSecurity
 @Slf4j
 public class SecurityConfig {
-    private final PasswordEncoder passwordEncoder;
-    private final JwtAuthenFilter jwtAuthenFilter;
-    private final UserDetailsService userDetailsService;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuthLoginSuccessHandler oAuthLoginSuccessHandler;
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(
-                        request -> request
-                                .anyRequest().permitAll()
-                )
-                .formLogin(login -> login.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthenFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(auth -> auth
-                                        .baseUri("/oauth2/authorize")
-                                // Nếu dùng cookie lưu redirect_uri:
-                                //.authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository())
-                        )
-                        .redirectionEndpoint(redir -> redir
-                                .baseUri("/oauth2/callback/*")
-                        )
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(customOAuth2UserService)
-                        )
-                        .successHandler(oAuthLoginSuccessHandler)
-                );
-        return http.build();
-    }
+        private final PasswordEncoder passwordEncoder;
+        private final JwtAuthenFilter jwtAuthenFilter;
+        private final UserDetailsService userDetailsService;
+        private final CustomOAuth2UserService customOAuth2UserService;
+        private final OAuthLoginSuccessHandler oAuthLoginSuccessHandler;
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8081"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("X-Auth-Token"));
-        configuration.setAllowCredentials(false);
-        configuration.setMaxAge(3600L);
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .csrf(csrf -> csrf.disable())
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                                .authorizeHttpRequests(
+                                                request -> request
+                                                                .anyRequest().permitAll())
+                                .formLogin(login -> login.disable())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authenticationProvider(authenticationProvider())
+                                .addFilterBefore(jwtAuthenFilter, UsernamePasswordAuthenticationFilter.class)
+                                .oauth2Login(oauth2 -> oauth2
+                                                .authorizationEndpoint(auth -> auth
+                                                                .baseUri("/oauth2/authorize")
+                                                // Nếu dùng cookie lưu redirect_uri:
+                                                // .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository())
+                                                )
+                                                .redirectionEndpoint(redir -> redir
+                                                                .baseUri("/oauth2/callback/*"))
+                                                .userInfoEndpoint(userInfo -> userInfo
+                                                                .userService(customOAuth2UserService))
+                                                .successHandler(oAuthLoginSuccessHandler));
+                return http.build();
+        }
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(List.of(
+                                "http://localhost:3000",
+                                "http://localhost:8081",
+                                "https://hackathon-fe-eqb2.onrender.com",
+                                "https://hackathon-be-uyzv.onrender.com"));
+                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(List.of("*"));
+                configuration.setExposedHeaders(List.of("X-Auth-Token"));
+                configuration.setAllowCredentials(false);
+                configuration.setMaxAge(3600L);
 
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder);
-        return provider;
-    }
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
+
+        @Bean
+        public AuthenticationProvider authenticationProvider() {
+                DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+                provider.setUserDetailsService(userDetailsService);
+                provider.setPasswordEncoder(passwordEncoder);
+                return provider;
+        }
 
 }
